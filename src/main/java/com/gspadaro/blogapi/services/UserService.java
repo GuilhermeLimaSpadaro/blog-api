e3b64a49f38f9cf53cb0bbb6015dc1aaad45bb1f
Guilherme Spadaro <guilhermespadaro@gmail.com>
2026-06-24 23:01:43 -0300

    feat: create user repository and user service

---
+++ src/main/java/com/gspadaro/blogapi/services/UserService.java
@@ -0,0 +1,23 @@
+package com.gspadaro.blogapi.services;
+
+import com.gspadaro.blogapi.domain.User;
+import com.gspadaro.blogapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
