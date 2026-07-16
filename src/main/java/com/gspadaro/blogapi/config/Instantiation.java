00663b1ba524d8d75d685fc8b9ec4c4c544c286a
Guilherme Spadaro <guilhermespadaro@gmail.com>
2026-07-15 21:47:37 -0300

    feat: update Instantiation to use AuthorDTO and new Post constructor

--- src/main/java/com/gspadaro/blogapi/config/Instantiation.java
+++ src/main/java/com/gspadaro/blogapi/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.gspadaro.blogapi.config;
 
 import com.gspadaro.blogapi.domain.Post;
 import com.gspadaro.blogapi.domain.User;
+import com.gspadaro.blogapi.dto.AuthorDTO;
 import com.gspadaro.blogapi.repository.PostRepository;
 import com.gspadaro.blogapi.repository.UserRepository;
 import org.jspecify.annotations.NonNull;
@@ -33,12 +34,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-
-        Post post00 = new Post(null, LocalDate.now(ZoneOffset.UTC), "Burguer king meu fast food favorito", "Adoro burguer king. Abraços!");
-        Post post01 = new Post(null, LocalDate.of(2018, 3, 21), "Partiu viagem", "Vou viajar para São Paulo. Abraços!");
-        Post post02 = new Post(null, LocalDate.of(2018, 3, 23), "Bom dia", "Acordei feliz hoje!");
-
         userRepo.saveAll(List.of(maria, alex, bob));
+
+        Post post00 = new Post(null, LocalDate.now(ZoneOffset.UTC), "Burguer king meu fast food favorito", "Adoro burguer king. Abraços!", AuthorDTO.from(maria));
+        Post post01 = new Post(null, LocalDate.of(2018, 3, 21), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", AuthorDTO.from(alex));
+        Post post02 = new Post(null, LocalDate.of(2018, 3, 23), "Bom dia", "Acordei feliz hoje!", AuthorDTO.from(bob));
+
         postRepo.saveAll(List.of(post00, post01, post02));
     }
 }
