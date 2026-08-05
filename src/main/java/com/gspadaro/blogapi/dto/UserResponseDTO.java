ab0801265560994ae93ccd8b041ace6a6b01b6a4
Guilherme Spadaro <guilhermespadaro@gmail.com>
2026-08-04 21:42:02 -0300

    feat(dto): include email in UserResponseDTO

--- src/main/java/com/gspadaro/blogapi/dto/UserResponseDTO.java
+++ src/main/java/com/gspadaro/blogapi/dto/UserResponseDTO.java
@@ -2,8 +2,8 @@ package com.gspadaro.blogapi.dto;
 
 import com.gspadaro.blogapi.domain.User;
 
-public record UserResponseDTO(String id, String name){
+public record UserResponseDTO(String id, String name, String email){
     public static UserResponseDTO from(User user){
-        return new UserResponseDTO(user.getId(), user.getName());
+        return new UserResponseDTO(user.getId(), user.getName(), user.getEmail());
     }
 }
