3e801b5ab70522bdb68d597f937182ae93884684
Guilherme Spadaro <guilhermespadaro@gmail.com>
2026-08-14 15:42:20 -0300

    feat: add postId to CommentResponseDTO and add id-aware constructor to Comment

--- src/main/java/com/gspadaro/blogapi/dto/CommentResponseDTO.java
+++ src/main/java/com/gspadaro/blogapi/dto/CommentResponseDTO.java
@@ -2,5 +2,5 @@ package com.gspadaro.blogapi.dto;
 
 import java.time.Instant;
 
-public record CommentResponseDTO(String id, String text, Instant date, UserDetailsDTO author) {
+public record CommentResponseDTO(String id, String text, Instant date, UserDetailsDTO author, String postId) {
 }
